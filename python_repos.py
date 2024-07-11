d9259ca9e578f79047d5c9c72c0606b0f86d717c
lukwac123 <lukasz-waclawek@wp.pl>
2024-07-11 23:01:26 +0200

    Wygenerowanie danych w postaci podsumowania.

--- python_repos.py
+++ python_repos.py
@@ -10,6 +10,15 @@ print(f"Kod stanu: {r.status_code}")
 
 # Konwersja obiektu odpowiedzi na słownik.
 response_dict = r.json()
+print(f"Całkowita liczba repozytoriów: {response_dict['total_count']}")
+print(f"Pełny zbiór wynikowy?: {not response_dict['incomplete_results']}")
 
-# Przetworzenie wyników.
-print(response_dict.keys())
+# Przetworzenie informacji o repozytoriach.
+repo_dicts = response_dict['items']
+print(f"Liczba zwróconych repozytoriów: {len(repo_dicts)}")
+
+# Przeanalizowanie pierwszego repozytorium.
+repo_dict = repo_dicts[0]
+print(f"\nKlucze: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
