641d2932f5a3be1598639db79e75491c15af44ae
lukwac123 <lukasz-waclawek@wp.pl>
2024-07-12 14:00:12 +0200

    Dodanie łączy do wykresu.

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -15,15 +15,26 @@ print(f"Pełny zbiór wynikowy?: {not response_dict['incomplete_results']}")
 
 # Przetworzenie informacji o repozytoriach.
 repo_dicts = response_dict['items']
-repo_names, stars = [], []
+repo_links, stars, hover_texts = [], [], []
 for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
+    # Zmiana nazwy repozytorium na łącze.
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
+
     stars.append(repo_dict['stargazers_count'])
+
+    # Przygotowanie podpowiedzi.
+    owner = repo_dict['owner'] ['login']
+    description = repo_dict['description']
+    hover_text = f"{owner}<br />{description}"
+    hover_texts.append(hover_text)
 
 # Utworzenie wizualizacji.
 title = "Oznaczone największą liczbą gwiazdek projekty Pythona w serwisie GitHub"
 labels = {'x': 'Repozytorium', 'y': 'Gwiazdki'}
-fig = px.bar(x=repo_names, y=stars, title=title, labels=labels)
+fig = px.bar(x=repo_links, y=stars, title=title, labels=labels, hover_name=hover_texts)
 
 fig.update_layout(title_font_size=28, xaxis_title_font_size=20, yaxis_title_font_size=20)
 fig.show()
